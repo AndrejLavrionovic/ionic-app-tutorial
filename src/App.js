990008220214dc7856a7c4060c0b366aa8909d9a
Andrej Lavrinovic <andrei.lavrinovic@gmail.com>
2021-10-09 20:32:30 +0100

    optimization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { IonButton, IonIcon, IonToast } from '@ionic/react';
+import { IonApp, IonButton, IonContent, IonHeader, IonIcon, IonTitle, IonToast, IonToolbar } from '@ionic/react';
 import { play as playIcon } from 'ionicons/icons';
 import React, { useState } from 'react';
 
@@ -10,18 +10,20 @@ function App() {
   };
 
   return (
-    <div>
-      <header className="App-header">
-        <h1>My app</h1>
-      </header>
-      <main>
+    <IonApp>
+      <IonHeader className="App-header">
+        <IonToolbar>
+          <IonTitle>My app</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
         <IonButton color="secondary" onClick={handleClick}>
           <IonIcon icon={playIcon} slot="start" />
           Click Me
         </IonButton>
         <IonToast isOpen={showToast} message="Hello World!" />
-      </main>
-    </div>
+      </IonContent>
+    </IonApp>
   );
 }
 
